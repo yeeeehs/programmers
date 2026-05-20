885ee04522f1fbde0c54a010f2c09bb1260c92e7
yeeeehs <leeihs122@gmail.com>
2026-05-21 08:55:49 +0900

    몫 구하기

---
+++ src/main/java/org/example/level0/DivideNumbers.java
@@ -0,0 +1,19 @@
+package org.example.level0;
+
+public class DivideNumbers {
+    public static void main(String[] args) {
+        /*
+        정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+         */
+
+        int num1 = 10;
+        int num2 = 5;
+
+        System.out.println(solution(num1, num2));
+    }
+
+    static int solution(int num1, int num2) {
+        int answer = num1 / num2;
+        return answer;
+    }
+}
