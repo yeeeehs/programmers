06cd1ab6ce97f6d7c1da7ae670396b109055af85
yeeeehs <leeihs122@gmail.com>
2026-05-20 22:09:56 +0900

    두 수의 합 구하기

---
+++ src/main/java/org/example/level0/TwoNumbersSum.java
@@ -0,0 +1,18 @@
+package org.example.level0;
+
+public class TwoNumbersSum {
+    public static void main(String[] args) {
+        /*
+        정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+         */
+
+        int num1 = 2;
+        int num2 = 3;
+        System.out.println(solution(num1, num2));
+    }
+
+    static int solution(int num1, int num2) {
+        int answer = num1 + num2;
+        return answer;
+    }
+}
