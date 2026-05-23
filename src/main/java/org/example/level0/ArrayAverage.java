e41531a299f9ed3502019cd16e3ac0f4cc300f68
yeeeehs <leeihs122@gmail.com>
2026-05-23 21:48:21 +0900

    배열의 평균값

---
+++ src/main/java/org/example/level0/ArrayAverage.java
@@ -0,0 +1,24 @@
+package org.example.level0;
+
+public class ArrayAverage {
+    public static void main(String[] args) {
+        /*
+        정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+         */
+
+        int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+
+        System.out.println(solution(numbers));
+    }
+
+    static double solution(int[] numbers) {
+
+        double answer = 0;
+
+        for(int i = 0; i < numbers.length; i++) {
+            answer += numbers[i];
+        }
+
+        return answer / numbers.length;
+    }
+}
