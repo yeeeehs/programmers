8587b96ca84030f1277b8f229cf7f2bb1f38860f
yeeeehs <leeihs122@gmail.com>
2026-05-27 20:32:41 +0900

    숫자 비교하기

--- src/main/java/org/example/level0/CompareNumbers.java
+++ src/main/java/org/example/level0/CompareNumbers.java
@@ -3,7 +3,8 @@ package org.example.level0;
 public class CompareNumbers {
     public static void main(String[] args) {
         /*
-        정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+        정수 num1과 num2가 매개변수로 주어집니다.
+        두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
          */
 
         int num1 = 2;
