d4d4c8e5d3aa72745d5420b1232c76bdc61e8184
yeeeehs <leeihs122@gmail.com>
2026-05-27 20:34:57 +0900

    두 수의 나눗셈

--- src/main/java/org/example/level0/MultiplyAfterDivision.java
+++ src/main/java/org/example/level0/MultiplyAfterDivision.java
@@ -3,7 +3,8 @@ package org.example.level0;
 public class MultiplyAfterDivision {
     public static void main(String[] args) {
         /*
-        정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 solution 함수를 완성해주세요.
+        정수 num1과 num2가 매개변수로 주어질 때,
+        num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 solution 함수를 완성해주세요.
          */
         int num1 = 3;
         int num2 = 2;
