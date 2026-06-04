ba41f841c64d201ea2f2e80b05870e35d0e8d800
yeeeehs <leeihs122@gmail.com>
2026-06-04 21:48:54 +0900

    모음 제거

---
+++ src/main/java/org/example/level0/RemoveVowels.java
@@ -0,0 +1,26 @@
+package org.example.level0;
+
+public class RemoveVowels {
+    public static void main(String[] args) {
+        /*
+        영어에선 a, e, i, o, u 다섯 가지 알파벳을 모음으로 분류합니다.
+        문자열 my_string이 매개변수로 주어질 때 모음을 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+         */
+
+        String my_string = "bus";
+
+        System.out.println(solution(my_string));
+    }
+
+    static String solution(String my_string) {
+        String answer = "";
+
+        my_string = my_string.replace("a", "");
+        my_string = my_string.replace("e", "");
+        my_string = my_string.replace("i", "");
+        my_string = my_string.replace("o", "");
+        my_string = my_string.replace("u", "");
+
+        return my_string;
+    }
+}
