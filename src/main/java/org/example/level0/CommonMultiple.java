434ab409f3a12d0ba46ca0fb780930877ceb8b7d
yeeeehs <leeihs122@gmail.com>
2026-07-07 20:06:45 +0900

    공배수

---
+++ src/main/java/org/example/level0/CommonMultiple.java
@@ -0,0 +1,25 @@
+package org.example.level0;
+
+public class CommonMultiple {
+    public static void main(String[] args) {
+        /*
+        정수 number와 n, m이 주어집니다.
+        number가 n의 배수이면서 m의 배수이면 1을 아니라면 0을 return하도록 solution 함수를 완성해주세요.
+         */
+
+        int number = 60;
+        int n = 2;
+        int m = 3;
+
+        System.out.println(solution(number, n, m));
+    }
+
+    static int solution(int number, int n, int m) {
+
+        if (number % n == 0 && number % m == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
