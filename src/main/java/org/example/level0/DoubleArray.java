cc0c1ed6f15ff4c55826084e4ee43f717733925e
yeeeehs <leeihs122@gmail.com>
2026-07-10 21:37:41 +0900

    배열 두배 만들기

---
+++ src/main/java/org/example/level0/DoubleArray.java
@@ -0,0 +1,23 @@
+package org.example.level0;
+
+import java.util.Arrays;
+
+public class DoubleArray {
+    public static void main(String[] args) {
+        /*
+        정수 배열 numbers가 매개변수로 주어집니다.
+        numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
+         */
+
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        System.out.println(Arrays.toString(solution(numbers)));
+    }
+
+    static int[] solution(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] *= 2;
+        }
+        return numbers;
+    }
+}
